d405da4651442c0f85644d412e0b2218b338198e
Osmary <Osmary@DESKTOP-8DOM8BM>
2024-02-08 22:49:50 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucle_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("imprimir numeros en la pantalla del 1 al 10");
+            Console.WriteLine();
+
+            for (int i = 1; i<= 10; i++)
+
+
+
+            {
+
+                Console.WriteLine("numero-  "+i);
+            }
+
+        }
+    }
+}
